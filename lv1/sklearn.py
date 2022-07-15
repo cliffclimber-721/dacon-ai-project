e1c77e340fd136a3261bbbe39cb131b7cc0a8dea
cliffclimber-721 <cliffclimber@g.skku.edu>
2022-07-15 19:35:53 +0900

    preprocessing

--- lv1/sklearn.py
+++ lv1/sklearn.py
@@ -1,7 +1,5 @@
 import pandas as pd
-import graphviz
 from sklearn import *
-from sklearn.tree import DecisionTreeRegressor
 
 traindata = pd.read_csv("/Users/skywalker721/Desktop/dacon-ai-project/lv1/riding/train.csv")
 testdata = pd.read_csv("/Users/skywalker721/Desktop/dacon-ai-project/lv1/riding/test.csv")
@@ -9,6 +7,6 @@ testdata = pd.read_csv("/Users/skywalker721/Desktop/dacon-ai-project/lv1/riding/
 X_train = traindata.drop(['count'], axis=1)
 Y_train = traindata['count']
 
-model = DecisionTreeRegressor()
+model = DecisionTreeClassifier()
 model.fit(X_train, Y_train)
 
